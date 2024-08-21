9d535aca75420360c19e5972f0409b818f2f889c
Samuel Hossain <samuel.hossain@outlook.com>
2024-08-21 15:08:16 -0400

    my car class exercise 1

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -3,28 +3,19 @@ namespace Classes;
 public class Car
 {
     
-    //constructors
-
-    public Car()
-    {
-        
-    }
-
-    public Car(string make, string model, int year)
-    {
-        Make = make;
-        Model = model;
-        Year = year;
-
-    }
-    
+ 
     
     
     //properties of a car
     public string Make { get; set; }
     public string Model { get; set; }
     public int Year { get; set; }
-    
+
+
+    public void Type()
+    {
+        Console.WriteLine($"The Car I have is a {Year} {Make} {Model} .");
+    }
     
     
 }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -9,25 +9,19 @@
             myCar.Model = "RX350";
             myCar.Year = 2015;
 
+            myCar.Type();
 
-            var nissan = new Car()
+
+            var brothersCar = new Car()
             {
                 Make = "Nissan",
                 Model = "Rogue",
                 Year = 2019
             };
-            
-            
-            var Ford = new Car("Ford", "Mustang", 2024);
 
-            var carList = new List<Car>{myCar, nissan, Ford};
+            brothersCar.Type();
 
-            foreach (var vehicle in carList)
-            {
-                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
-            }
-            
-            
+
 
 
 
